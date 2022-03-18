78268a7a6941ba11e6b754d5673102de3446d4f5
HardyWisnu <hardywisnu@students.amikom.ac.id>
2022-03-18 14:46:09 +0700

    Isi Main

--- CalculatorGitHub/CalculatorGitHub/Program.cs
+++ CalculatorGitHub/CalculatorGitHub/Program.cs
@@ -2,6 +2,14 @@
 {
     static void Main(String[] args)
     {
+        int a = 10;
+        int b = 6;
+
+        Console.WriteLine("Hasil Penambahan : {0} + {1} = {2}", a, b, Penambahan(a, b));
+        Console.WriteLine("Hasil Penambahan : {0} - {1} = {2}", a, b, Pengurangan(a, b));
+
+        Console.WriteLine("\nTekan Key untuk keluar");
+        Console.ReadKey();
 
     }
     
